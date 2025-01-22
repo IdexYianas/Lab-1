156460da5e7f3ebd8b13d6ff632b17bd39d2b328
IdexYianas <giancagorostiza@gmail.com>
2025-01-22 15:39:30 +0000

    Fichier 1 Exercice 3

--- Exercice3.c
+++ Exercice3.c
@@ -1,9 +1,9 @@
 #include <stdio.h> //directive pour la biblioteque standar
 // Function principale
-main(){
+int main(){
     char nom[] = "Giancarlo"; //Declaracion de la variable type "chain"
 
-    printf ("Bonjour, %s \n" , nom[]); //Afiche le message. Le %s est pour aficher le valeur de la variable
+    printf ("Bonjour, %s \n" , nom); //Afiche le message. Le %s est pour aficher le valeur de la variable
 
     return 0; //fin
 }
